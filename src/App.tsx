08ee923bfd455344d78015c11f27a51c6835c95f
cuzekn <cuzekn@gmail.com>
2025-11-05 14:50:16 +0900

    feat: Toasterを追加し、通知機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { Toaster } from 'react-hot-toast';
+
 import { Router } from './routes/Router';
 
 export default function App() {
   return (
     <div>
       <Router />
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+          success: {
+            duration: 3000,
+            iconTheme: {
+              primary: '#4ade80',
+              secondary: '#fff',
+            },
+          },
+          error: {
+            duration: 4000,
+            iconTheme: {
+              primary: '#ef4444',
+              secondary: '#fff',
+            },
+          },
+        }}
+      />
     </div>
   );
 }
