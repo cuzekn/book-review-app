ff129a700ecaec23cd4cba01a60b1ebf3ed856aa
cuzekn <cuzekn@gmail.com>
2025-11-12 00:52:33 +0900

    feat: Paginationコンポーネントを追加し、Homeページにページネーション機能を実装

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import './index.css';
 
 import App from './App.tsx';
+import { store } from './store';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </StrictMode>,
   );
 }
